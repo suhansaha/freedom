d72e84e04aed79b96bf9c62719a24df88857c99d
Suhan Saha <suhansaha@gmail.com>
2019-05-23 00:42:52 +0530

    Some playing with the dash

--- app.py
+++ app.py
@@ -1,29 +1,79 @@
-import os
+# -*- coding: utf-8 -*-
 import dash
-
 import dash_core_components as dcc
-import dash_html_components as html 
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from datetime import datetime as dt
+import dash_daq as daq
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-server = app.server
-
 app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
+    dcc.Tabs(id="tabs", value='tab-2', children=[
+        dcc.Tab(label='Backtesting', value='tab-1'),
+        dcc.Tab(label='Live Trading', value='tab-2'),
+    ]),
+    html.Div(id='tabs-content')
 ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs', 'value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div([
+            html.H3('Backtesting'),
+            dcc.Dropdown(
+                options=[
+                    {'label': 'New York City', 'value': 'NYC'},
+                    {'label': 'Montréal', 'value': 'MTL'},
+                    {'label': 'San Francisco', 'value': 'SF'}
+                ],
+                multi=True,
+                value='MTL'
+            ),
+            dcc.DatePickerRange(
+                id='date-picker-range',
+                start_date=dt(1997, 5, 3),
+                end_date_placeholder_text='Select a date!'
+            ),
+            dcc.Checklist(
+                options=[
+                    {'label': 'New York City', 'value': 'NYC'},
+                    {'label': 'Montréal', 'value': 'MTL'},
+                    {'label': 'San Francisco', 'value': 'SF'}
+                ],
+                values=['MTL', 'SF'],
+                labelStyle={'display': 'inline-block'}
+            ),
+            dcc.RadioItems(
+                options=[
+                    {'label': 'New York City', 'value': 'NYC'},
+                    {'label': 'Montréal', 'value': 'MTL'},
+                    {'label': 'San Francisco', 'value': 'SF'}
+                ],
+                value='MTL',
+                labelStyle={'display': 'inline-block'}
+            ),
+            html.Button('Submit', id='button')
+        ])
+    elif tab == 'tab-2':
+        return html.Div([
+            html.H3('Live Trading'),
+            dcc.Dropdown(
+                options=[
+                    {'label': 'New York City', 'value': 'NYC'},
+                    {'label': 'Montréal', 'value': 'MTL'},
+                    {'label': 'San Francisco', 'value': 'SF'}
+                ],
+                multi=True,
+                value='MTL'
+            ),
+            html.Button('Submit', id='button')
+        ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
